07e3dccca5663065f56d1c9e4fc2165f409ab6f8
wonjongKim <kwjdnjs@gmail.com>
2023-02-15 12:35:48 +0900

    update tx api

--- src/btc/btc.controller.ts
+++ src/btc/btc.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { query } from 'express';
 import { BtcService } from './btc.service';
 
 @Controller('btc')
@@ -12,16 +13,16 @@ export class BtcController {
 
   @Get('addr/:id')
   getAddrInfo(@Param('id') id: string) {
-    return this.btcService.getData(id, 'addr');
+    return this.btcService.getData(id, 'addr', false);
   }
 
   @Get('tx/:id')
-  getTxInfo(@Param('id') id: string) {
-    return this.btcService.getData(id, 'tx');
+  getTxInfo(@Param('id') id: string, @Query('light') isLight: boolean) {
+    return this.btcService.getData(id, 'tx', isLight);
   }
 
   @Get('block/:id')
   getBlockInfo(@Param('id') id: string) {
-    return this.btcService.getData(id, 'block');
+    return this.btcService.getData(id, 'block', false);
   }
 }
--- src/btc/btc.service.ts
+++ src/btc/btc.service.ts
@@ -11,7 +11,7 @@ export class BtcService {
   constructor(private readonly httpService: HttpService) {}
 
   // get data from electrum json rpc
-  async getRawData(id: string, reqType: IdType) {
+  async getRawData(id: string, reqType: IdType, isLight: boolean) {
     const client = new ElectrumClient('electrum.bitaroo.net', 50002, 'ssl');
     await client.connect(
       'electrum-client-js', // optional client name
@@ -28,7 +28,38 @@ export class BtcService {
       );
       data['tx_hashes'] = data['tx_hashes'].reverse();
     } else if (reqType == 'tx') {
-      data = await client.blockchain_transaction_get(id, true);
+      const tx_data = await client.blockchain_transaction_get(id, true);
+
+      // vin
+      const vin = tx_data['vin'];
+      const fullVinList = [];
+      for (const i in vin) {
+        const prevTx = await client.blockchain_transaction_get(
+          vin[i]['txid'],
+          true,
+        );
+
+        const fullVin = prevTx['vout'][vin[i]['vout']];
+        if (isLight) {
+          fullVin['addresses'] = fullVin['scriptPubKey']['addresses'];
+          delete fullVin.scriptPubKey;
+        } else {
+          fullVin['scriptSig'] = vin[i]['scriptSig'];
+          fullVin['sequence'] = vin[i]['sequence'];
+        }
+        fullVinList.push(fullVin);
+      }
+      data['vin'] = fullVinList;
+
+      // vout
+      const vout = tx_data['vout'];
+      if (isLight) {
+        for (const i in vout) {
+          vout[i]['addresses'] = vout[i]['scriptPubKey']['addresses'];
+          delete vout[i].scriptPubKey;
+        }
+      }
+      data['vout'] = vout;
     } else if (reqType == 'block') {
       data = await client.blockchain_block_header(id, parseInt(id) + 1);
     } else {
@@ -46,7 +77,7 @@ export class BtcService {
 
     for (const i in idTypeList) {
       try {
-        await this.getRawData(id, idTypeList[i]);
+        await this.getRawData(id, idTypeList[i], false);
         idInfo['idType'] = idTypeList[i];
         break;
       } catch {
@@ -56,12 +87,12 @@ export class BtcService {
     return idInfo;
   }
 
-  async getData(id: string, idType: IdType) {
+  async getData(id: string, idType: IdType, isLight: boolean) {
     const idInfo: IdInfo = { network: 'btc', id, idType };
     const data: Data = { idInfo, data: {} };
     try {
-      data['data'] = await this.getRawData(id, idType);
-    } catch {
+      data['data'] = await this.getRawData(id, idType, isLight);
+    } catch (e) {
       return { error: 'unknown type error' };
     }
 
